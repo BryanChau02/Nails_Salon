8f0e7c84157069a56eaa7f0453c2a7a010ad8ae0
Bryan Chau <bchau2020@fau.edu>
2025-06-07 00:43:32 +0000

    1st java

---
+++ Nail Salon/app.js
@@ -0,0 +1,28 @@
+ocument.querySelectorAll(".form-control").forEach(function(elm) {
+    elm.addEventListener("change", function(e) {
+      // <- add a listener to every input
+      const attribute = e.target.getAttribute("for"); // when any input changes, collect the value
+      let values = {};
+      values[attribute] =
+        this.value == "" || this.value == "null"
+          ? null
+          : this.value == "true"
+          ? true
+          : this.value == "false"
+          ? false
+          : this.value;
+      render(Object.assign(window.variables, values)); // render again the card with new values
+    });
+  });
+
+
+
+let variables = {
+    name: "",
+    email: "",
+    phone: "",
+    subject: "",
+    message: ""
+}
+
+console.log(name);
